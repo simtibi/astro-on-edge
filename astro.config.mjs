adfd93994734dc6928c1ed17e37e673b948e7aae
tiborsimonbene <152640553+tiborsimonbene@users.noreply.github.com>
2024-03-02 17:21:31 +0100

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/edge-functions";
-
-// import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify({
+    // Options are passed directly to `@netlify/plugin-functions-core`
     edgeMiddleware: true,
   }),
 });
